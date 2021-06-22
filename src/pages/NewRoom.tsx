e7b7d49abca421bc72dd0a019f1326ee554d6e28
lauro-silva44 <allvinhos@gmail.com>
2021-06-22 22:49:00 -0100

    add room using FormEvent

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,5 +1,4 @@
 //import { useAuth } from "../hooks/useAuth";
-
 import illustration from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import { Button } from "../components/Button";
@@ -7,10 +6,22 @@ import { Link} from "react-router-dom"
 
 
 import '../styles/auth.scss'
+import { useState, FormEvent } from "react";
 
 export function NewRoom() {
  
-//   const { user } = useAuth() 
+//   const { user } = useAuth()
+const [newRoom, setNewRoom] = useState('');
+
+async function handleCreateRoom(event: FormEvent) {
+
+  event.preventDefault();
+
+  if(newRoom.trim() === ''){
+    return;
+  }
+}
+
   return ( 
     <div id = "page-auth" >
       <aside>
@@ -28,9 +39,11 @@ export function NewRoom() {
           <img src={logoImg} alt="Letmeask" />
           <h2> Criar uma nova sala</h2>
 
-          <form>
+          <form onSubmit={handleCreateRoom}>
             <input type="text" 
-            placeholder=" Digite o código da sala" />
+            placeholder=" Digite o código da sala" 
+            onChange ={event => setNewRoom(event.target.value)}
+            value = {newRoom} />
 
             <Button type="submit"> 
                Criar sala 
