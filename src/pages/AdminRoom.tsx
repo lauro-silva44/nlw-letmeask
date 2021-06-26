0f806f4676f9ced919b7d729ed6161f8dd29ab1c
lauro-silva44 <allvinhos@gmail.com>
2021-06-26 01:25:50 -0100

    add remove button 4 ?s

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 
 import { Button } from "../components/Button";
 import logoImg from "../assets/images/logo.svg";
@@ -18,11 +18,21 @@ type RoomParams = {
 
 export function AdminRoom() {
    // const { user } = useAuth();
+   const history = useHistory();
     const params = useParams<RoomParams>();
    // const [newQuestion, setNewQuestion] = useState("");
     const roomId = params.id;
+    
 
     const { title, questions} = useRoom(roomId);
+
+    async function handleEndRoom() {
+        await database().ref(`rooms/${roomId}`).update({
+            endedAt: new Date(), 
+        })
+
+        history.push(`/`);
+    }
 
     async function handleDeleteQuestion(questionId: string) {
         if(window.confirm('Tem certeza que você deseja excluir esta pergunta?')){
@@ -37,7 +47,7 @@ export function AdminRoom() {
                     <img src={logoImg} alt="letmeask" />
                     <div>
                         <RoomCode code={roomId} />
-                        <Button isOutlined > Encerrar sala </Button>
+                        <Button isOutlined onClick={handleEndRoom} > Encerrar sala </Button>
                     </div>
                 </div>
             </header>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -38,6 +38,10 @@ export function Home() {
       alert('Room does not exist.')
       return;
     }
+    if(roomRef.val().endedAt){
+      alert('room already closed.')
+      return;
+    }
 
     history.push(`/rooms/${roomCode}`)
   }
